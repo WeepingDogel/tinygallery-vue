e99874c31a80ab5fafc2ec069359500a809aeaa1
WeepingDogel <weepingdogel@gmail.com>
2023-02-14 00:00:15 +0800

    Environment Changed.

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/tests': {
+        target: 'http://127.0.0.1:8000',
+      },
+    },
+  },
 })
