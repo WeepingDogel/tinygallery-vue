8d5a1749a2b93fa58faf0facfaaa09e49e88f98b
WeepingDogel <weepingdogel@gmail.com>
2023-02-14 21:02:57 +0800

    Remark Page, which is used to display the full-size Image.

--- src/views/RemarkView.vue
+++ src/views/RemarkView.vue
@@ -8,7 +8,7 @@ export default{
     <div class="RemarkContainer">
         <div class="RemarkBox">
             <div class="ImageDisplayArea">
-
+                <img src="" />
             </div>
             <div class="RemarksArea">
 
@@ -18,18 +18,51 @@ export default{
 </template>
 
 <style>
+@keyframes FadeIn {
+    from {
+        opacity: 0;
+        top: 10px;
+    }
+
+    to {
+        opacity: 1;
+        top: 0px;
+    }
+}
+
 .RemarkContainer{
     width: 100%;
     height: 100vh;
     display: flex;
     justify-content: center;
+    animation: FadeIn 0.5s;
 }
 
 .RemarkBox{
-    width: 1500px;
-    height: 100vh;
+    width: 100%;
+    height: 90vh;
     box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
-    margin: 10px;
+    margin: 15px;
     border-radius: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+    transition: 0.5s ease-in-out;
 }
+
+.ImageDisplayArea{
+    width: 80%;
+    height: 90vh;
+    background-color: #3F51B5;
+    border-top-left-radius: 5px;
+}
+
+.RemarksArea{
+    width: 20%;
+    height: 90vh;
+    background-color: #C5CAE9;
+    border-top-right-radius: 5px;
+}
+
 </style>
