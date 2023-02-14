fdfdb9b468a357e8d861b3419db6e632728a5c30
WeepingDogel <weepingdogel@gmail.com>
2023-02-14 14:04:45 +0800

    Remark Page.

--- src/views/RemarkView.vue
+++ src/views/RemarkView.vue
@@ -1,11 +1,35 @@
 <script lang="ts">
+export default{
 
+}
 </script>
 
 <template>
+    <div class="RemarkContainer">
+        <div class="RemarkBox">
+            <div class="ImageDisplayArea">
 
+            </div>
+            <div class="RemarksArea">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <style>
+.RemarkContainer{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+}
 
+.RemarkBox{
+    width: 1500px;
+    height: 100vh;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+    margin: 10px;
+    border-radius: 5px;
+}
 </style>
