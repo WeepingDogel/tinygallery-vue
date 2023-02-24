98b1ee3b80cb3f6cf3adbd3fcc83b169f87775bd
WeepingDogel <weepingdogel@gmail.com>
2023-02-24 20:10:13 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <script lang="ts">
 import NavBar from './components/navbar.vue'
 import { RouterView } from 'vue-router'
+import { Authentication } from './stores/Authentication';
 export default {
-  components: {
-    NavBar
-  }
+    data() {
+        return {
+            RouterURL: "",
+            LinkName: ""
+        }
+    },
+    mounted() {
+        if (Authentication().CheckIfLogged) {
+            this.RouterURL = "/profile";
+            this.LinkName = "Profile";
+        } else {
+            this.RouterURL = "/login";
+            this.LinkName = "Login"
+        }
+    },
+    components: {
+        NavBar
+    }
 }
 </script>
 
 <template>
-  <NavBar />
-  <RouterView />
+    <NavBar :dynamicRouterLocate="RouterURL" :dynamicLinkName="LinkName" />
+    <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
