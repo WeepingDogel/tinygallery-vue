432eaeba8eb4e190a6a3aa92b875e10e6d295324
WeepingDogel <weepingdogel@gmail.com>
2023-02-25 17:21:24 +0800

    Uploader Panel is ready.

---
+++ src/components/Upload/Uploader.vue
@@ -0,0 +1,33 @@
+<!-- @/components/Upload/Uploader.vue -->
+<script lang="ts">
+import { Authentication } from '@/stores/Authentication';
+
+export default {
+    
+}
+</script>
+
+<template>
+    <div class="Mask">
+        <div class="UploaderPanel">
+
+        </div>
+    </div>
+</template>
+
+<style>
+
+.Mask {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.UploaderPanel {
+    width: 400px;
+    height: 500px;
+    background-color: #FFFFFF;
+}
+</style>
