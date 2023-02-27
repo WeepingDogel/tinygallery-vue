4d2e58733aaa0f54706c628fbdd51ca3b8a33474
WeepingDogel <weepingdogel@gmail.com>
2023-02-27 17:06:10 +0800

    About Information

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -21,13 +21,25 @@ export default {
                 <br />
                 And this one is made as a vue edition.
             </p>
-        </div>
-        <div class="SystemInfoPart">
-            <h1 class="AboutTitle">
-                System Information
-            </h1>
+            <h1 class="AboutTitle">Technology Stack</h1>
             <p class="AboutPara">
-                TinyGallery: {{ Version }}
+                <img src="https://img.shields.io/badge/vuejs-%2335495e.svg?style=for-the-badge&amp;logo=vuedotjs&amp;logoColor=%234FC08D"
+                    alt="Vue.js">
+                <img src="https://img.shields.io/badge/Vuetify-1867C0?style=for-the-badge&amp;logo=vuetify&amp;logoColor=AEDDFF"
+                    alt="Vuetify">
+                <img src="https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&amp;logo=vite&amp;logoColor=white"
+                    alt="Vite">
+                <img src="https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&amp;logo=fastapi" alt="FastAPI">
+                <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&amp;logo=python&amp;logoColor=ffdd54" alt="Python">
+            </p>
+            <h1 class="AboutTitle">Source Code</h1>
+            <p class="AboutPara">
+                Vue Front End: <br />
+                <a href="https://github.com/WeepingDogel/tinygallery-vue"><img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&amp;logo=github&amp;logoColor=white" alt="GitHub"></a>
+            </p>
+            <p class="AboutPara">
+                Backend: <br />
+                <a href="https://github.com/WeepingDogel/tinygallery-backend"><img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&amp;logo=github&amp;logoColor=white" alt="GitHub"></a>
             </p>
         </div>
     </div>
@@ -36,7 +48,7 @@ export default {
 <style>
 .AboutContainer {
     width: 100%;
-    height: 100vh;
+    height: auto;
     display: flex;
     flex-direction: column;
     align-items: start;
@@ -54,7 +66,7 @@ export default {
 }
 
 .IntroductionPart .Logo {
-    width: 400px;
+    width: 800px;
     height: auto;
     margin: 0 auto;
     display: block;
@@ -75,15 +87,5 @@ export default {
     padding: 20px;
     text-align: justify;
     line-height: 25px;
-}
-
-.SystemInfoPart {
-    width: 80%;
-    height: 50vh;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
-    transition: 0.5s ease-in-out;
-    margin: 10px auto;
-    border-radius: 10px;
-    animation: FadeIn 1s;
 }
 </style>
