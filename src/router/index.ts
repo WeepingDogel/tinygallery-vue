85b4eb7272c734d7bb15746843cd5421eeed40f0
WeepingDogel <weepingdogel@gmail.com>
2023-03-20 22:36:09 +0800

    Add router rules of remark page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const router = createRouter({
       component: () => import('../views/ProfileView.vue')
     },
     {
-      path: '/remark',
+      path: '/remark/:post_uuid',
       name: 'Remark',
       component: () => import('../views/RemarkView.vue')
     }
